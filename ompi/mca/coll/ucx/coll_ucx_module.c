7bb2bb63219f42a3f917b0ec8d3ae469a48c4deb
public (843ed2ad0e21) <993835762@qq.com>
2020-12-18 21:21:18 +0800

    Solve the problem of program jam when one socket is balanced and the other is unbalanced

--- ompi/mca/coll/ucx/coll_ucx_module.c
+++ ompi/mca/coll/ucx/coll_ucx_module.c
@@ -381,6 +381,23 @@ static void mca_coll_ucg_arg_free(struct ompi_communicator_t *comm, ucg_group_pa
     }
 }
 
+static void mca_coll_ucg_init_is_socket_balance(ucg_group_params_t *group_params, mca_coll_ucx_module_t *module)
+{
+    unsigned pps = ucg_builtin_calculate_ppx(group_params, UCG_GROUP_MEMBER_DISTANCE_SOCKET);
+    unsigned ppn = ucg_builtin_calculate_ppx(group_params, UCG_GROUP_MEMBER_DISTANCE_HOST);
+    char is_socket_balance = (pps == (ppn - pps) || pps == ppn);
+    char result = is_socket_balance;
+    int status = ompi_coll_base_allreduce_intra_basic_linear(&is_socket_balance, &result, 1, MPI_CHAR, MPI_MIN,
+                                                             MPI_COMM_WORLD, &module->super);
+    if (status != OMPI_SUCCESS) {
+        int error = MPI_ERR_INTERN;
+        COLL_UCX_ERROR("ompi_coll_base_allreduce_intra_basic_linear failed");
+        ompi_mpi_errors_are_fatal_comm_handler(NULL, &error, "Failed to init is_socket_balance");
+    }
+    group_params->is_socket_balance = result;
+    return ;
+}
+
 static int mca_coll_ucg_create(mca_coll_ucx_module_t *module, struct ompi_communicator_t *comm)
 {
     ucs_status_t error;
@@ -454,6 +471,7 @@ static int mca_coll_ucg_create(mca_coll_ucx_module_t *module, struct ompi_commun
         goto out;
     }
  
+    mca_coll_ucg_init_is_socket_balance(&args, module);
     error = ucg_group_create(mca_coll_ucx_component.ucg_worker, &args, &module->ucg_group);
 
     /* Examine comm_new return value */
